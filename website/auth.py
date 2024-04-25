375fb8d43f86777e8ef81d01c0bcae8a53873b52
Vincent Mei <vincentmei291@gmail.com>
2024-04-25 16:18:17 -0700

    login page html commit

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,16 @@
 from flask import Blueprint
+from flask import render_template
+from flask import redirect
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return redirect('/login')
 
 @auth.route('/sign-up')
 def sign_up():
--- website/view.py
+++ website/view.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
+from flask import Flask, redirect
 
 view = Blueprint('view', __name__)
 
 @view.route('/')
-def home():
-    return "<h1>Test</h1>"
+def default():
+    return redirect('/login')
